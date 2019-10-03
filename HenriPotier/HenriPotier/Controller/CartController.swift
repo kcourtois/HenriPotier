8f0e5ebe1365b2922c5d941d509952730d45aaed
kcourtois <courtois.k2@gmail.com>
2019-10-03 15:41:17 +0200

    Xcode project created/setup, basic API call added

---
+++ HenriPotier/HenriPotier/Controller/CartController.swift
@@ -0,0 +1,30 @@
+//
+//  CartController.swift
+//  HenriPotier
+//
+//  Created by Kévin Courtois on 03/10/2019.
+//  Copyright © 2019 Kévin Courtois. All rights reserved.
+//
+
+import UIKit
+
+class CartController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
